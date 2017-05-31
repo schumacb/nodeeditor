51bc0028912857c5966911fb30b9cf3608b69f2b
Bastian Schumacher <Bastian.Schumacher@gmx.de>
2017-05-31 10:19:19 +0200

    NodeModel and SimpleNodeModel added

---
+++ src/NodeModel.cpp
@@ -0,0 +1,23 @@
+#include "NodeModel.hpp"
+
+using namespace QtNodes;
+
+NodeModel::NodeModel() {}
+
+void NodeModel::setInData(std::shared_ptr<QtNodes::NodeData> nodeData,
+                          QtNodes::PortIndex port) {
+  Q_UNUSED(nodeData)
+  Q_UNUSED(port)
+}
+
+std::shared_ptr<NodeData> NodeModel::outData(PortIndex port) {
+  Q_UNUSED(port)
+  std::shared_ptr<NodeData> ptr;
+  return ptr;
+}
+
+SimpleNodeModel::SimpleNodeModel() {}
+
+QString SimpleNodeModel::caption() const { return ""; }
+
+bool SimpleNodeModel::captionVisible() const { return false; }
---
+++ src/NodeModel.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QtCore/QObject>
+#include <QtCore/qglobal.h>
+
+#include "NodeDataModel.hpp"
+namespace QtNodes {
+
+/// Simplified version of a NodeDataModel where setInData and outData are not
+/// used. Eg. in a headless mode.
+class NODE_EDITOR_PUBLIC NodeModel : public NodeDataModel {
+  Q_OBJECT
+public:
+  NodeModel();
+  virtual ~NodeModel() = default;
+
+  /// setInData should not be used
+  void setInData(std::shared_ptr<NodeData> nodeData, PortIndex port);
+  /// outData should not be used
+  std::shared_ptr<NodeData> outData(PortIndex port);
+};
+
+/// Further simplified version of a NodaModel wher no caption is used.
+class NODE_EDITOR_PUBLIC SimpleNodeModel : public NodeModel {
+  Q_OBJECT
+public:
+  SimpleNodeModel();
+  virtual ~SimpleNodeModel() = default;
+
+  /// returns an empty string as caption
+  QString caption() const;
+
+  /// caption is invisible by default
+  bool captionVisible() const;
+};
+}
